8fcf1a8414bf34406dbd9ec9d4b9af9a43089a6b
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-02 00:13:16 -0800

    Add functionality to clock

--- src/clock.jsx
+++ src/clock.jsx
@@ -1,5 +1,43 @@
-class Clock extends React.Component {
+import React from 'react';
 
+class Clock extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            time: new Date()
+        };
+        this.int = null;
+    }
+
+    tick () {
+        this.setState({
+            time: new Date()
+        });
+    }
+
+    componentDidMount () {
+        this.int = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillMount () {
+        clearInterval(this.int);
+    }
+
+    render () {
+        return (
+            <section className='clock'>
+                <h1>Clock</h1>
+                <span className='time'>
+                    <h1>Time: </h1>
+                    <h1>{this.state.time.toLocaleTimeString()}</h1>
+                </span>    
+                <span className='date'>
+                    <h1>Date: </h1>
+                    <h1>{this.state.time.toDateString()}</h1>
+                </span>    
+            </section>
+        );
+    }
 }
 
 export default Clock;
