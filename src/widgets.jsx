bc1fc29e1f849dc61e4aca4fa1a7d0055690dcc9
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-01 16:38:07 -0800

    Initial commit

---
+++ src/widgets.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(<h1>You Done Messed Up A-Aron</h1>, root);
+
+    ReactDOM.render(
+        <App />,
+        root
+    );
+})
+
+function App () {
+    return (
+        <div>
+
+        </div>
+    );
+}
